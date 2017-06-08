d18c972a6a1c74a325756af6e965d8c31401037a
rosiehighsmith <rosie.highsmith@gmail.com>
2017-06-08 07:41:08 -0700

    Initial commit, create daysOfWeek array.

---
+++ collections.cs/collections.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collections.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monday = "Monday";
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+        }
+    }
+}
